189237a2efe14a4ea1cfffc3709f30ef5168d21c
amwatah <jackalltrade2020@gmail.com>
2022-05-02 13:38:53 +0300

    added footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,11 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+     <div data-Theme="night" className="site min-h-screen font-mono w-screen overflow-x-hidden">
+       <Component {...pageProps} />
+     </div>
+  )
 }
 
 export default MyApp
